d27c4642dd9322fbc04c0ae0c7c417f8c39b14e6
Gaurav Kathiriya <153881067+gauravk45@users.noreply.github.com>
2023-12-16 17:31:15 +0530

    Add files via upload

---
+++ Day-03/QR_Generator.py
@@ -0,0 +1,26 @@
+import pyqrcode
+import png
+from PIL import ImageTk, Image
+from pyqrcode import QRCode
+import tkinter as tk 
+from tkinter import *
+import PIL.Image
+
+window = Tk()  
+window.geometry('300x350')
+window.title('PythonGeeks')
+
+Label(window,text='Lets Create QR Code',font='arial 15').pack()
+
+s = tk.StringVar()
+
+
+def create_qrcode():
+    s1=s.get()
+    qr = pyqrcode.create(s1)
+    qr.png('myqr.png', scale = 6)
+    Label(window,text='QR Code is created and saved successfully').pack()
+
+Entry(window,textvariable=s,font='arial 15').pack()
+Button(window,text='create',bg='pink',command=create_qrcode).pack()
+window.mainloop()
